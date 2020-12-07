ecddccc41b6e0a769c06ad8cf8ec4439a7e17114
prakarsh <prakar.vastava@gmail.com>
2020-12-07 21:39:00 +0530

    Added text animation on name

--- app.js
+++ app.js
@@ -66,3 +66,8 @@ container.addEventListener('mouseleave', (e)=>{
     c9.style.transform = "translateZ(0px)"
     c10.style.transform = "translateZ(0px)"
 })
+
+const name = document.querySelectorAll("#name path");
+for(let i=0; i<name.length; i++){
+    console.log(`Letter ${i} is ${name[i].getTotalLength()}`);
+}
